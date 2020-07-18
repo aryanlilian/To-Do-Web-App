7f9a04fd536bc62b1da8552469a95a9171699a1a
Bularu Lilian <cryaryan@gmail.com>
2020-07-18 14:50:08 +0300

    Created Task table in database

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
